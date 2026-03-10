6e669cd4a600f3e70bb2cc1bec07069975c079a2
K0brin <nlundy2099@gmail.com>
2026-03-10 15:36:49 -0500

    raycast works on target

--- Source/GunSmith_Nlundy/Private/PrimaryRifle.cpp
+++ Source/GunSmith_Nlundy/Private/PrimaryRifle.cpp
@@ -33,7 +33,10 @@ void APrimaryRifle::Tick(float DeltaTime)
 
 void APrimaryRifle::FireWeapon()
 {
-	FVector barrelTipLocation = {0,0,0};
+	FCollisionQueryParams params;
+	params.AddIgnoredActor(this); // Ignores the Rifle
+	params.AddIgnoredActor(GetWorld()->GetFirstPlayerController()->GetPawn()); // Ignores the Player holding it
+	FVector barrelTipLocation = {100,100,100};
 	//get line trace start
 	TArray<UStaticMeshComponent*> meshCompArray;
 	GetComponents<UStaticMeshComponent>(meshCompArray);
@@ -47,27 +50,44 @@ void APrimaryRifle::FireWeapon()
 	
 	FHitResult hitResult1;
 	FHitResult hitResult;
-	FVector lineTraceStart = {barrelTipLocation.X,barrelTipLocation.Y,barrelTipLocation.Z};
+	FVector lineTraceStart = barrelTipLocation;
 	FVector playerLineStart = GetWorld()->GetFirstPlayerController()->GetPawn()->GetComponentByClass<UCameraComponent>()->GetComponentLocation();
 	//calculating dynamic forward
 	FVector lineTraceEnd = lineTraceStart + (GetActorForwardVector() * -LineTraceDistance);
 	//call function on for each bullet - raycast is held here
 	//raycast from center of player (to get target position)
-	GetWorld()->LineTraceSingleByChannel (hitResult1, playerLineStart, lineTraceEnd,ECC_Visibility);
+	GetWorld()->LineTraceSingleByChannel (hitResult1, playerLineStart, lineTraceEnd,ECC_Visibility, params);
 	DrawDebugLine(GetWorld(),playerLineStart, hitResult1.Location, FColor::Green, false, 1.0f, 0, 2.0f);
 	//raycast from gun
-	GetWorld()->LineTraceSingleByChannel(hitResult, lineTraceStart, hitResult1.Location,ECC_Visibility);
+	GetWorld()->LineTraceSingleByChannel(hitResult, lineTraceStart, hitResult1.ImpactPoint,ECC_Visibility, params);
 	DrawDebugLine(GetWorld(),lineTraceStart, hitResult.Location, FColor::Red, false, 1.0f, 0, 2.0f);
+	AActor* hitActor1 = hitResult1.GetActor();
 	AActor* hitActor = hitResult.GetActor();
+	
+	if (hitActor1)
+	{
+		FString actor1Name = hitActor1->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Hit1 Actor: %s"), *actor1Name);
+		UE_LOG(LogTemp, Warning, TEXT("Hit1 Location: %s"), *hitResult1.Location.ToString());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit1 Nothing"));
+		UE_LOG(LogTemp, Warning, TEXT("Hit1 Location: %s"), *hitResult1.Location.ToString());
+		
+	}
+	
 	if (hitActor)
 	{
 		FString actorName = hitActor->GetName();
-		UE_LOG(LogTemp, Warning, TEXT("Hit1 Actor: %s"), *actorName);
-		
+		UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s"), *actorName);
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *hitResult.Location.ToString());
 	}
 	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Hit Nothing"));
+		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *hitResult.Location.ToString());
+		
 	}
 	
 }
